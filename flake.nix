25234ecdfcea2ba5a27f49559120408d918e88a2
Vladimir Fetisov <vladimir.fetisov@pm.me>
2025-02-11 20:46:00 +0100

    add dev scripts to devShell

--- flake.nix
+++ flake.nix
@@ -15,12 +15,38 @@
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
+
+        cmdPkg = "cmd/nix-search-tv";
+
+        mkScript = name: text: pkgs.writeShellScriptBin name text;
+        scripts = [
+          (mkScript "build" "go build -o $DEV_DIR/bin $CMD_DIR")
+          (mkScript "run" "$DEV_DIR/bin/nix-search-tv $@ --config $DEV_DIR/config.json")
+          (mkScript "print-search" "run print")
+          (mkScript "preview-search" "run preview $@")
+
+          (mkScript "build-n-tv" "build && print-search | tv --preview 'echo {} | awk \"{ print $1$2 }\" | xargs preview-search'")
+          (mkScript "build-n-fzf" "build && print-search | fzf --preview 'preview-search {1}{2}'")
+        ];
+
       in {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             go_1_23
             gopls
+            brotli
+            television
+            fzf
+            tmux
           ];
+
+          buildInputs = scripts;
+
+          shellHook = ''
+            export PROJECT_ROOT=$(git rev-parse --show-toplevel)
+            export DEV_DIR="$PROJECT_ROOT/.dev"
+            export CMD_DIR="$PROJECT_ROOT/${cmdPkg}"
+          '';
         };
 
         packages.default = pkgs.buildGo123Module {
@@ -31,7 +57,7 @@
           # vendorHash = nixpkgs.lib.fakeHash;
           vendorHash = "sha256-uzNDhkovlXx0tIgSJ3E08d0TNmktSrlOOe8Iwi4ZfmU=";
 
-          subPackages = ["cmd/nix-search-tv"];
+          subPackages = [cmdPkg];
 
           meta = {
             description = "A tool integrating television and nix-search packages";
