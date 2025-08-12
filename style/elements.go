897c87f6e23ff7e7f459b1fa982056157518fc62
Vladimir Fetisov <vladimir.fetisov@pm.me>
2025-08-12 20:07:31 +0200

    remove one of PrintCodeBlock functions
    V2 drawed boxes with box-drawing characters and only working with fzf.
    Since 0.11.8, television also supports these characters, so the V1
    function is not needed anymore

--- style/elements.go
+++ style/elements.go
@@ -156,39 +156,7 @@ func styleCallouts(text string) string {
 }
 
 // PrintCodeBlock prints the given content inside a styled code block
-//
-// TODO: at the moment of writing, television couldn't correctly
-// render box-drawing characters, so use +, | and - for now. Once
-// there is a fix, change the lines to those below:
-//
-//	topBorder := "┌" + strings.Repeat("─", lineWidth-2) + "┐"
-//	bottomBorder := "└" + strings.Repeat("─", lineWidth-2) + "┘"
-//	leftBorder := "│ "
-//	rightBorder := " │"
 func PrintCodeBlock(content string) string {
-	for _, env := range os.Environ() {
-		if strings.HasPrefix(env, "FZF_PREVIEW") {
-			return PrintCodeBlockV2(content)
-		}
-	}
-
-	// Determine the maximum width of the content
-	lineWidth := findMaxWidth(content) + 4 // Add padding for borders
-	topBorder := "+" + strings.Repeat("-", lineWidth-2) + "+"
-	bottomBorder := "+" + strings.Repeat("-", lineWidth-2) + "+"
-	leftBorder := "| "
-	rightBorder := " |"
-
-	block := topBorder + "\n"
-	for _, line := range strings.Split(content, "\n") {
-		paddedLine := fmt.Sprintf("%-*s", lineWidth-4, line) // Ensure lines fit inside the box
-		block += leftBorder + paddedLine + rightBorder + "\n"
-	}
-
-	return block + bottomBorder
-}
-
-func PrintCodeBlockV2(content string) string {
 	// Determine the maximum width of the content
 	lineWidth := findMaxWidth(content) + 4 // Add padding for borders
 	topBorder := "┌" + strings.Repeat("─", lineWidth-2) + "┐"
