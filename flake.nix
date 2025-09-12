ab770690cdddca65fb7a563da7b99a50655d2142
Vladimir Fetisov <vladimir.fetisov@pm.me>
2025-09-12 17:45:48 +0200

    rename integrations to tests
    I think that name is more intuitive and correct
    
    update gitignore
    
    update script

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
           (mkScript "print-search" "run print")
           (mkScript "preview-search" "run preview $@")
 
-          (mkScript "test-integrations" "build && NIX_SEARCH_TV_BIN=$DEV_DIR/bin/nix-search-tv go test --count 1 -v ./integrations/...")
+          (mkScript "test-integrations" "build && NIX_SEARCH_TV_BIN=$DEV_DIR/bin/nix-search-tv go test --count 1 -v ./tests/...")
 
           (mkScript "build-n-tv" "build && print-search | tv --preview-command 'preview-search {}'")
           (mkScript "build-n-fzf" "build && print-search | fzf --wrap --preview 'preview-search {}' --preview-window=wrap --scheme=history")
